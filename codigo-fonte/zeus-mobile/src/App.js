4895b8ccdd448e24013f0f3a731f8af67388a7fb
NataliaRS01 <natalia.romero@sga.pucminas.br>
2025-10-30 13:55:06 -0300

    Frontend registro de peso e ajustes

--- codigo-fonte/zeus-mobile/src/App.js
+++ codigo-fonte/zeus-mobile/src/App.js
@@ -1,23 +1,49 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import HomeScreen from './pages/HomeScreen';
 import RegistrarPeso from './pages/RegistrarPeso'; 
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+     <Drawer.Navigator
         initialRouteName="Home"
+        // Configurações globais do Drawer
         screenOptions={{
-          headerShown: false, 
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RegistrarPeso" component={RegistrarPeso} />
-        {/* Adicione outras telas aqui (ex: CalcularIMC) */}
-      </Stack.Navigator>
+          headerShown: true, 
+          headerStyle: {
+            backgroundColor: '#FF8C00', 
+          },
+          headerTintColor: '#fff', 
+          drawerActiveTintColor: '#FF8C00', 
+          drawerInactiveTintColor: '#333',
+        }}       
+      >
+      <Drawer.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Início', headerShown: false,
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+          }} 
+        />
+       <Drawer.Screen 
+          name="RegistrarPeso" 
+          component={RegistrarPeso} 
+          options={{ title: 'Registrar Peso', headerShown: false,
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="scale-outline" size={size} color={color} /> 
+            ),
+           }} 
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
---
+++ codigo-fonte/zeus-mobile/src/styles/FooterStyle.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const FooterStyle = StyleSheet.create({
+
+    footer: {
+        height: 40,
+        backgroundColor: '#FF8C00', 
+    } ,
+    });
+
+export default FooterStyle;
