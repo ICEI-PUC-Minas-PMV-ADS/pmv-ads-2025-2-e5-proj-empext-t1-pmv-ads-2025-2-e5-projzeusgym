a41e532dc08c529a3fe24d350759316ed0cb384b
NataliaRS01 <natalia.romero@sga.pucminas.br>
2025-11-08 11:43:57 -0300

    Alteração index

--- codigo-fonte/src/models/index.js
+++ codigo-fonte/src/models/index.js
@@ -1,35 +1,47 @@
-const sequelize = require('../config/database');
+// src/models/index.js
 const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Caminho para o arquivo onde você cria o Sequelize
 
+// Importa os modelos
+const Users = require('./Users')(sequelize, DataTypes);
+const Exercises = require('./Exercises')(sequelize, DataTypes);
+const Weight = require('./Weight')(sequelize, DataTypes);
+const TrainingSheet = require('./TrainingSheet')(sequelize, DataTypes);
+const TrainingSheetExercises = require('./TrainingSheetExercises')(sequelize, DataTypes);
+const PhysicalAssessment = require('./PhysicalAssessment')(sequelize, DataTypes);
+
+// Cria o objeto db
 const db = {
-    sequelize,
-    Users,
-    Exercises,
-    Weight,
+  sequelize,
+  Users,
+  Exercises,
+  Weight,
+  TrainingSheet,
+  TrainingSheetExercises,
+  PhysicalAssessment,
 };
 
-db.Users = require('./Users')(sequelize, DataTypes);
-db.Exercises = require('./Exercises')(sequelize, DataTypes);
-db.Weight = require('./Weight')(sequelize, DataTypes);
-
-db.TrainingSheet = require('./TrainingSheet')(sequelize, DataTypes);
-db.TrainingSheetExercises = require('./TrainingSheetExercises')(sequelize, DataTypes);
-db.PhysicalAssessment = require('./PhysicalAssessment')(sequelize, DataTypes);
-
 // Associações
-// 🛑 MODIFICADO: Adicionado 'as: "exercises"' para corrigir seu erro
-db.TrainingSheet.belongsToMany(db.Exercises, { through: db.TrainingSheetExercises, foreignKey: 'sheetId', otherKey: 'exerciseId', as: 'exercises' });
-db.Exercises.belongsToMany(db.TrainingSheet, { through: db.TrainingSheetExercises, foreignKey: 'exerciseId', otherKey: 'sheetId' }); // Pode ou não precisar do alias 'as: "sheets"'
+db.TrainingSheet.belongsToMany(db.Exercises, {
+  through: db.TrainingSheetExercises,
+  foreignKey: 'sheetId',
+  otherKey: 'exerciseId',
+  as: 'exercises'
+});
 
-// 🛑 ADICIONADO: Associações BelongsTo para aluno e professor da Ficha de Treino
+db.Exercises.belongsToMany(db.TrainingSheet, {
+  through: db.TrainingSheetExercises,
+  foreignKey: 'exerciseId',
+  otherKey: 'sheetId'
+});
+
 db.TrainingSheet.belongsTo(db.Users, { as: 'aluno', foreignKey: 'alunoId' });
 db.TrainingSheet.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
 
-// Associações para PhysicalAssessment
 db.PhysicalAssessment.belongsTo(db.Users, { as: 'student', foreignKey: 'studentId' });
 db.PhysicalAssessment.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
 
-db.Users.hasMany(db.Weight, {foreignKey: 'userId', as: 'weightHistory'});
-db.Weight.belongsTo(db.Users, {foreignKey: 'userId', as: 'user'});
+db.Users.hasMany(db.Weight, { foreignKey: 'userId', as: 'weightHistory' });
+db.Weight.belongsTo(db.Users, { foreignKey: 'userId', as: 'user' });
 
 module.exports = db;
