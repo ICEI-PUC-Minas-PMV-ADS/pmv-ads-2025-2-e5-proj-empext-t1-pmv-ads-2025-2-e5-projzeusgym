56be162e6473c42b1f34bdef481f863e72cad160
NataliaRS01 <natalia.romero@sga.pucminas.br>
2025-11-08 11:58:04 -0300

    Ajsutes

--- codigo-fonte/src/models/index.js
+++ codigo-fonte/src/models/index.js
@@ -1,61 +1,75 @@
 const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
-// 🔹 Função auxiliar para importar modelos
-function loadModel(modelPath) {
-  const modelDef = require(modelPath);
-  // Se o model exporta uma função, chamamos com (sequelize, DataTypes)
-  return typeof modelDef === 'function' ? modelDef(sequelize, DataTypes) : modelDef;
-}
+const db = {};
 
-// 🔹 Importa todos os models corretamente
-const Users = loadModel('./Users');
-const Exercises = loadModel('./Exercises');
-const Weight = loadModel('./Weight');
-const TrainingSheet = loadModel('./TrainingSheet');
-const TrainingSheetExercises = loadModel('./TrainingSheetExercises');
-const PhysicalAssessment = loadModel('./PhysicalAssessment');
+// Caminho da pasta de models
+const modelsDir = __dirname;
 
-// 🔹 Cria o objeto db
-const db = {
-  sequelize,
-  Users,
-  Exercises,
-  Weight,
-  TrainingSheet,
-  TrainingSheetExercises,
-  PhysicalAssessment,
-};
+// Carrega todos os models dinamicamente
+fs.readdirSync(modelsDir)
+  .filter((file) => file !== 'index.js' && file.endsWith('.js'))
+  .forEach((file) => {
+    const modelPath = path.join(modelsDir, file);
+    const modelDef = require(modelPath);
 
-// 🔹 Executa associações declaradas dentro dos models (ex: TrainingSheet.associate)
+    let model;
+
+    // 🔹 Suporte para os dois padrões de exportação
+    if (typeof modelDef === 'function' && !(modelDef.prototype instanceof sequelize.Model)) {
+      // Forma antiga: exporta uma função (sequelize, DataTypes)
+      model = modelDef(sequelize, DataTypes);
+    } else if (modelDef.prototype instanceof sequelize.Model) {
+      // Forma moderna: classe estende Model
+      model = modelDef.initModel(sequelize);
+    } else {
+      // Já é um model definido
+      model = modelDef;
+    }
+
+    db[model.name] = model;
+  });
+
+// 🔹 Configura associações (se existirem)
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-// 🔹 Associações adicionais globais (caso não estejam nos models)
-db.TrainingSheet.belongsToMany(db.Exercises, {
-  through: db.TrainingSheetExercises,
-  foreignKey: 'sheetId',
-  otherKey: 'exerciseId',
-  as: 'exercises',
-});
+// 🔹 Associações extras específicas (se não estiverem dentro dos models)
+if (db.TrainingSheet && db.Exercises && db.TrainingSheetExercises) {
+  db.TrainingSheet.belongsToMany(db.Exercises, {
+    through: db.TrainingSheetExercises,
+    foreignKey: 'sheetId',
+    otherKey: 'exerciseId',
+    as: 'exercises',
+  });
 
-db.Exercises.belongsToMany(db.TrainingSheet, {
-  through: db.TrainingSheetExercises,
-  foreignKey: 'exerciseId',
-  otherKey: 'sheetId',
-});
+  db.Exercises.belongsToMany(db.TrainingSheet, {
+    through: db.TrainingSheetExercises,
+    foreignKey: 'exerciseId',
+    otherKey: 'sheetId',
+  });
+}
 
-db.TrainingSheet.belongsTo(db.Users, { as: 'aluno', foreignKey: 'alunoId' });
-db.TrainingSheet.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
+if (db.TrainingSheet && db.Users) {
+  db.TrainingSheet.belongsTo(db.Users, { as: 'aluno', foreignKey: 'alunoId' });
+  db.TrainingSheet.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
+}
 
-db.PhysicalAssessment.belongsTo(db.Users, { as: 'student', foreignKey: 'studentId' });
-db.PhysicalAssessment.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
+if (db.PhysicalAssessment && db.Users) {
+  db.PhysicalAssessment.belongsTo(db.Users, { as: 'student', foreignKey: 'studentId' });
+  db.PhysicalAssessment.belongsTo(db.Users, { as: 'professor', foreignKey: 'professorId' });
+}
 
-db.Users.hasMany(db.Weight, { foreignKey: 'userId', as: 'weightHistory' });
-db.Weight.belongsTo(db.Users, { foreignKey: 'userId', as: 'user' });
+if (db.Users && db.Weight) {
+  db.Users.hasMany(db.Weight, { foreignKey: 'userId', as: 'weightHistory' });
+  db.Weight.belongsTo(db.Users, { foreignKey: 'userId', as: 'user' });
+}
 
-// 🔹 Exporta o objeto db
+db.sequelize = sequelize;
+
 module.exports = db;
