0db3c849a57544fd04ef5b287cc478b232c33013
NataliaRS01 <natalia.romero@sga.pucminas.br>
2025-11-17 19:12:19 -0300

    Ajustes

--- codigo-fonte/zeus-web/src/services/api.js
+++ codigo-fonte/zeus-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // URL do SEU back-end
+    baseURL: 'https://guarded-shelf-40573-5295222ff305.herokuapp.com', // URL do SEU back-end
 });
 
 export default api;
