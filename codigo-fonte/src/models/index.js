cfdc63cae4a5a49c61e7878b358acea2c02fd27d
DtKimura <dkimura@sga.pucminas.br>
2025-11-19 15:13:00 -0300

    ok

--- codigo-fonte/src/models/index.js
+++ codigo-fonte/src/models/index.js
@@ -1,36 +1,12 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
-
+const sequelize = require('../config/database');
 const { Sequelize, DataTypes } = require('sequelize');
 
-
-let sequelize; // A variável sequelize será definida abaixo
-
-// Configuração do banco (Heroku ou local)
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    dialect: 'mysql',
-    logging: false,
-  });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      logging: false,
-    }
-  );
-}
-
-const Users = require('./Users'); // Agora importa o modelo (classe) diretamente
-const Exercises = require('./Exercises');
-const PhysicalAssessment = require('./PhysicalAssessment');
-const TrainingSheet = require('./TrainingSheet');
-const TrainingSheetExercises = require('./TrainingSheetExercises');
-const Weight = require('./Weight');
+const Users = require('./Users')(sequelize, DataTypes);
+const Exercises = require('./Exercises')(sequelize, DataTypes);
+const PhysicalAssessment = require('./PhysicalAssessment')(sequelize, DataTypes);
+const TrainingSheet = require('./TrainingSheet')(sequelize, DataTypes);
+const TrainingSheetExercises = require('./TrainingSheetExercises')(sequelize, DataTypes);
+const Weight = require('./Weight')(sequelize, DataTypes);
 
 
 if (Users.associate) Users.associate({ Exercises, PhysicalAssessment, TrainingSheet, Weight });
