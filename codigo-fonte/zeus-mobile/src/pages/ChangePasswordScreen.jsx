eaa6890af0936aafa8c9054f3b35ebbfee807be8
DtKimura <dkimura@sga.pucminas.br>
2025-11-20 04:22:53 -0300

    ok

--- codigo-fonte/zeus-mobile/src/pages/ChangePasswordScreen.jsx
+++ codigo-fonte/zeus-mobile/src/pages/ChangePasswordScreen.jsx
@@ -38,7 +38,7 @@ export default function ChangePasswordScreen({ navigation, route }) {
         setIsLoading(true);
 
         try {
-            const response = await fetch(`${API_BASE_URL}/auth/change-password`, {
+            const response = await fetch('https://teste-zeusgym-50b8de268016.herokuapp.com/auth/change-password', {
                 method: 'POST', // Seu endpoint deve usar PUT ou POST
                 headers: {
                     'Content-Type': 'application/json',
