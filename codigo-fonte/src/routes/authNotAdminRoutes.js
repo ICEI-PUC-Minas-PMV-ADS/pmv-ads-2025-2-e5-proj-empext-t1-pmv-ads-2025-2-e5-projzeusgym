011e1d3978fcbe76c1954d40c905677fdcce98ca
DtKimura <dkimura@sga.pucminas.br>
2025-11-25 01:49:59 -0300

    correção

--- codigo-fonte/src/routes/authNotAdminRoutes.js
+++ codigo-fonte/src/routes/authNotAdminRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authNotAdminController');
+const authNotAdminMiddleware = require('../middlewares/authNotAdminMiddleware');
 
 router.post('/login', authController.userLogin);
 
 router.post('/change-password', authController.changePassword);
 
 // Rotas protegidas (requerem autenticação)
-router.get('/profile', authMiddleware, authController.getProfile)
-router.put('/profile', authMiddleware, authController.updateProfile)
-router.delete('/profile', authMiddleware, authController.deleteProfile)
+router.get('/profile', authNotAdminMiddleware, authController.getProfile)
+router.put('/profile', authNotAdminMiddleware, authController.updateProfile)
+router.delete('/profile', authNotAdminMiddleware, authController.deleteProfile)
 
 module.exports = router;
